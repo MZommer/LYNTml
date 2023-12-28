c34eaa232406bbf36f0eaa665c3ea28f1f6d3c1b
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-12-29 01:37:10 +0200

    Update Logger.py

--- src/LYN/Logger.py
+++ src/LYN/Logger.py
@@ -4,8 +4,16 @@ logger = logging.getLogger('LyN')
 
 logger.setLevel(logging.INFO)
 
+# Create a handler
+handler = logging.StreamHandler()
+
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger.setFormatter(formatter)
+
+# Set the formatter to the handler, not the logger
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(handler)
 
 def toggle_level():
     if logger.level == logging.DEBUG:
